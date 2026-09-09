ed507f6fe8c5d091cb2cd9f26dd8f0be4857477b
Vitor Rocha <vitor.rocha.bcc@gmail.com>
2026-09-09 14:56:42 -0300

    Initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <h1 className="">
+          App
+        </h1>
+      </main>
+    </div>
+  );
+}
